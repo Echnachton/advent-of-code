0e3dafa8e717741d73108355d1d7b3676a27a958
Echnachton <example@example.com>
2024-12-03 09:02:36 +0100

    updated code for day 1

--- 2024/day1/main.ts
+++ 2024/day1/main.ts
@@ -1,3 +1,24 @@
 import { getDataForDay } from "@utils";
 
 const data = await getDataForDay("1");
+
+const listOne: number[] = [];
+const listTwo: number[] = [];
+
+data.split(/\n/).map((pair: string) => {
+    const [a, b] = pair.split(/\s+/)
+    if (!!a && !!b) {
+        listOne.push(parseInt(a));
+        listTwo.push(parseInt(b));
+    }
+});
+
+listOne.sort();
+listTwo.sort();
+
+let distance = 0;
+for (let i = 0; i < listOne.length; i++) {
+    distance += Math.abs(listOne[i] - listTwo[i]);
+}
+
+console.log(distance);
